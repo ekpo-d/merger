c01e32bfa66f465c5e6acaa0a5d9668050454589
Ekpo David <ekpodave@gmail.com>
2017-06-15 15:37:37 +0100

    removed comments

--- dev/frontend/assets/js/controllers/app.js
+++ dev/frontend/assets/js/controllers/app.js
@@ -32,38 +32,3 @@ app.run(['$rootScope', function($rootScope) {
         $rootScope.title = current.$$route.title;
     });
 }]);
-
-// var app = angular.module('app', ['ngRoute', 'chieffancypants.loadingBar','truncate','ngAnimate', 'ui.bootstrap',  'loginModule', 'homeModule'])
-
-// app.config(function($routeProvider, $locationProvider){
-//   $routeProvider
-//     .when('/merged', {
-//       title: 'Merged',
-//       templateUrl: 'dashboard.html',
-//       controller: 'HomeCtrlr'
-//     })
-//     .when('/login', {
-//       title: 'Welcome',
-//       templateUrl: 'login.html',
-//       controller: 'LoginCtrlr'
-//     })
-//     .when('/conflicts', {
-//       title: 'Conflicts',
-//       templateUrl: 'dashboard.html',
-//       controller: 'HomeCtrlr'
-//     })
-//     .when('/logs', {
-//       title: 'Logs',
-//       templateUrl: 'dashboard.html',
-//       controller: 'HomeCtrlr'
-//     })
-//     .otherwise({redirectTo: '/merged'})
-
-//   $locationProvider.html5Mode(true);
-// })
-
-// app.run(['$rootScope', function($rootScope) {
-//     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
-//         $rootScope.title = current.$$route.title;
-//     });
-// }]);
